4135b48d7e457bd561eecb884fb685217d538589
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-14 12:24:39 +0530

    [Biplab] Add: Creating a table

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -1,2 +1,10 @@
 create database EmpPayRollService;
 use EmpPayRollService; 
+create table EmployeeTable
+(
+ID int identity(1,1) primary key,
+Name varchar(10) not null,
+Address varchar(10),
+Salary int,
+StartDate date
+)
